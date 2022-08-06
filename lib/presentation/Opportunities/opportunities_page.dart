38d11c6b083541494780f0eb989f827f1dfaf67b
sadeghbiglar <sbmail555@gmail.com>
2022-08-06 06:36:29 -0700

    part 13 folders and dependencies complete

---
+++ lib/presentation/Opportunities/opportunities_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OpportunitiesPage extends StatefulWidget {
+  @override
+  _OpportunitiesPageState createState() => _OpportunitiesPageState();
+}
+
+class _OpportunitiesPageState extends State<OpportunitiesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home Page')),
+    );
+  }
+}
